6118f6e7235d055f3f942bf0e8460c276273e769
Untold Coding <113816767+vishaltewatia@users.noreply.github.com>
2023-09-23 11:24:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const cursor = document.querySelector('.cursor');
-const rays = document.querySelector('.rays');
+const cursor = document.querySelector(".cursor");
+const rays = document.querySelector(".rays");
 
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
+// Function to move the cursor
+function moveCursor(x, y) {
+  cursor.style.left = x - cursor.clientWidth / 2 + "px";
+  cursor.style.top = y - cursor.clientHeight / 2 + "px";
+}
 
-    cursor.style.left = x - cursor.clientWidth / 2 + 'px';
-    cursor.style.top = y - cursor.clientHeight / 2 + 'px';
+// Function to create and add a ray
+function createRay(x) {
+  const ray = document.createElement("div");
+  ray.classList.add("ray");
+  ray.style.left = x + "px";
+  ray.style.top = "0px";
 
-    // Create and add a ray
-    const ray = document.createElement('div');
-    ray.classList.add('ray');
-    ray.style.left = x + 'px';
-    ray.style.top = '0px';
+  rays.appendChild(ray);
 
-    rays.appendChild(ray);
+  // Remove the ray after the animation
+  ray.addEventListener("animationend", () => {
+    ray.remove();
+  });
+}
 
-    // Remove the ray after the animation
-    ray.addEventListener('animationend', () => {
-        ray.remove();
-    });
+// Mousemove event
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  moveCursor(x, y);
+  createRay(x);
+});
+
+// Touch events
+document.addEventListener("touchmove", (e) => {
+  const touch = e.touches[0]; // Get the first touch
+  const x = touch.clientX;
+  const y = touch.clientY;
+  moveCursor(x, y);
+  createRay(x);
 });
